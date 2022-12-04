b9802767a3285f71b4b274bcd3200459c05ddf45
ed.xyz <91804938+EDED2314@users.noreply.github.com>
2022-12-04 09:14:56 -0500

    redundency

--- ports.py
+++ ports.py
@@ -10,6 +10,12 @@ def dataPort(name: str, baudrate: int):
     ser = serial.Serial(name, baudrate)
     ser.timeout = 10
     
+    while True:
+        line = str(ser.readline(), "utf-8")
+        print("python LOG: " + line)
+        if(line.startswith("#")):
+            break
+    
     print(f"python: getting data - will take a few seconds")
     lines = str(ser.readall(), "utf-8").split("\n")
     print("\npython: finished reading data - writing data\n")
@@ -17,7 +23,7 @@ def dataPort(name: str, baudrate: int):
 
 
     for i in progressbar(range(len(lines)), "Writing data: ",  40):
-        time.sleep(0.1)
+        time.sleep(0.001)
 
     print("python: wrapping up...")
     writeData(lines)
