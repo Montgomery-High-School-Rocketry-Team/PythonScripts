29d329dfff589e908bfbf05b8da505b226e511da
ed.xyz <91804938+EDED2314@users.noreply.github.com>
2022-12-04 08:24:21 -0500

    consideration that no wfif

--- data.bat
+++ data.bat
@@ -1,5 +1,6 @@
 @echo off
 title data
+echo make sure you have pyserial installed! (pip install pyserial)
 echo data coming in soon -> press enter to start data transfer
 python main.py
 echo done, please exit :D and find data.csv somewhere in the directory!
--- data.sh
+++ data.sh
@@ -1,6 +1,7 @@
 #!/bin/sh 
 # script to execute file
 cd "$(dirname "$0")"
+echo "make sure you have pyserial installed! (pip install pyserial)"
 echo "----RUNNING SCRIPTS----"
 echo "~~~~~This may take up to ~20 seconds to offload~~~~~"
 python "$(pwd)/main.py"
