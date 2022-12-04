cc0d343b6a33fa34273d201aee8c5da352a4d638
ed.xyz <91804938+EDED2314@users.noreply.github.com>
2022-12-04 13:05:47 -0500

    ui improvement

--- data.bat
+++ data.bat
@@ -1,7 +1,7 @@
 @echo off
 title data
 echo make sure you have pyserial installed! (pip install pyserial)
-echo data coming in soon -> press enter to start data transfer
+echo data coming in soon -> press enter to start data transfer (will be around 20 seconds)
 python main.py
 echo done, please exit :D and find data.csv somewhere in the directory!
 pause
