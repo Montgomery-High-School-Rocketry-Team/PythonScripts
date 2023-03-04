c301556d5c2a1618cbcfe609b40882f9d4c44ca5
Eddie <eddietang2314@gmail.com>
2023-03-04 15:49:49 -0500

    more serial support

--- ports.py
+++ ports.py
@@ -9,12 +9,28 @@ DECODE = 'utf-8'
 def dataPort(name: str, baudrate: int):
     ser = serial.Serial(name, baudrate)
     ser.timeout = 10
+    hashtag_count = 0
+    lines = []
     
     while True:
-        line = str(ser.readline(), "utf-8")
-        #print("python LOG: " + line)
-        if(line.startswith("#")):
+        if(hashtag_count == 0):
+            line = str(ser.readline(), "utf-8")
+            #print("python LOG: " + line)
+            if(line.startswith("##")):
+                hashtag_count += 1
+                
+        elif (hashtag_count == 1):
+            lines.append( str(ser.readline(), "utf-8"))
+            if(line.startswith("##")):
+                hashtag_count += 1
+        
+        elif (hashtag_count == 2):
             break
+        else:
+            break
+        
+            
+        
     
     print(f"python: getting data - will take a few seconds")
     lines = str(ser.readall(), "utf-8").split("\n")
