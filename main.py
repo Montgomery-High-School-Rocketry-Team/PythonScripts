f0151900b9203e84bdf5d89281d3d0d5574a1154
Eddie <eddietang2314@gmail.com>
2023-03-04 16:07:13 -0500

    change

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from ports import *
 # TODO find a method to determine the correct port
 #TODO i just remembered i need to make sure that  the team member has termainl set as the running thing (brian and I)
 #WAIT 10 SECONDS FOR DATA TO COME OVER
-dataPort(listPort()[0], 9600)
+dataPort(listPort()[0], 115200)
