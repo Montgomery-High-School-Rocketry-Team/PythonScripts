ad9b1c6d1b2d641f265499306e278cb6c02ea643
Eddie <eddietang2314@gmail.com>
2024-02-08 17:43:23 -0500

    asdfdasf

--- visualize.py
+++ visualize.py
@@ -1,12 +1,40 @@
 import pandas as pd
 import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 df = pd.read_csv("data.csv")
 
-fig = px.line(df, x="#time", y="ax", title="time vs vel")
-fig.show()
 
-figg = px.line(df, x="#time", y="ay", title="time vs ax")
-figg.show()
-figgg = px.line(df, x="#time", y="az", title="time vs ax")
-figgg.show()
+fig = make_subplots(rows=2, cols=3)
+
+fig.add_trace(
+    go.Scatter(x=df["#time"], y=df["vel"], mode="lines", name="time vs velocity"),
+    row=1,
+    col=1,
+)
+fig.add_trace(
+    go.Scatter(x=df["#time"], y=df["alt"], mode="lines", name="time vs alt"),
+    row=1,
+    col=2,
+)
+
+fig.add_trace(
+    go.Scatter(x=df["#time"], y=df["ax"], mode="lines", name="time vs ax"),
+    row=2,
+    col=1,
+)
+fig.add_trace(
+    go.Scatter(x=df["#time"], y=df["ay"], mode="lines", name="time vs ay"),
+    row=2,
+    col=2,
+)
+fig.add_trace(
+    go.Scatter(x=df["#time"], y=df["ay"], mode="lines", name="time vs ay"),
+    row=2,
+    col=3,
+)
+
+
+fig.update_layout(title_text="Subplots")
+fig.show()
