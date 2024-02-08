0beadb80983b6cb6fc0c02e82525edd1c326621d
Eddie <eddietang2314@gmail.com>
2024-02-08 16:34:27 -0500

    adsf

--- visualize.py
+++ visualize.py
@@ -3,5 +3,5 @@ import plotly.express as px
 
 df = pd.read_csv("data.csv")
 
-fig = px.line(df, x="#time", y="alt", title="time vs alt")
+fig = px.line(df, x="#time", y="vel", title="time vs alt")
 fig.show()
